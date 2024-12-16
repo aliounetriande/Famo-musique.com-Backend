7ff4ba5f438287bd7f6ed7eda3ef7b472d9984be
Marie24-star <mariebado295@gmail.com>
2024-12-16 12:49:30 +0000

    update

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,6 +3,6 @@ const { addNewUser, userLogin } = require("../controllers/usersControllers");
 const router = express.Router();
 
 router.post("/sign-up", addNewUser);
-router.post("/login", userLogin);
+router.post("/login", userLogin)
 
 module.exports = router;
